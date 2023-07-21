9d28e349ad9d8aca7c55531defe98b0b8f529282
Ibrahim Ahmed <abeahmed2@gmail.com>
2023-07-21 16:02:19 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="context-sdk",
-    version="0.0.1",
+    version="0.0.2",
     description="Context SDK",
     author="Context",
     author_email="sam@usecontext.com",
